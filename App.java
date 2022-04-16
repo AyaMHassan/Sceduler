83bd0c7d3fcbd82d8a91bdfe6b07ff415bd3fa97
AyaMHassan <54334085+AyaMHassan@users.noreply.github.com>
2022-04-16 21:45:02 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,10 +1,13 @@
-package com.example.demo2;
+package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
@@ -12,19 +15,27 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import java.io.IOException;
 
-import org.jfree.chart.ChartPanel;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.IntervalCategoryDataset;
 import org.jfree.data.gantt.Task;
 import org.jfree.data.gantt.TaskSeries;
 import org.jfree.data.gantt.TaskSeriesCollection;
-import org.jfree.data.time.Millisecond;
 import org.jfree.data.time.SimpleTimePeriod;
-import org.jfree.data.time.TimePeriod;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
 
     String[] Array = new String[4];
     String type = new String();
@@ -67,7 +78,7 @@ public class HelloApplication extends Application {
 
         SJF_Pre.setOnMouseClicked(e ->{
             try{
-                type = "SJF_Pre";
+                type = "SJF_pre";
                 Array = display();
             }
             catch(Exception er1){
@@ -169,7 +180,25 @@ public class HelloApplication extends Application {
         avg_wait.setOnMouseClicked((new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 try {
-                    time_display(100);
+                    if(type == "FCFS"){
+                        time_display(100);
+                    }
+                    else if(type == "SJF_pre"){
+                        time_display(100);
+                    }
+                    else if(type == "SJF_non"){
+                        time_display(100);
+                    }
+                    else if(type == "priority_pre"){
+                        time_display(100);
+                    }
+                    else if(type == "priority_non"){
+                        time_display(100);
+                    }
+                    else if(type == "RR"){
+                        time_display(100);
+                    }
+
                 } catch (Exception er1) {
                     er1.printStackTrace();
                 }
@@ -179,8 +208,51 @@ public class HelloApplication extends Application {
         gantt.setOnMouseClicked((new EventHandler<MouseEvent>(){
             public void handle(MouseEvent event) {
                 try {
-                    gantt_display();
-                }catch (Exception er1) {
+                    if(type == "FCFS"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+                    else if(type == "SJF_pre"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+                    else if(type == "SJF_non"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+                    else if(type == "priority_pre"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+                    else if(type == "priority_non"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+                    else if(type == "RR"){
+                        gantt_display(
+                                new String[]{"p1", "p2", "p3", "p4"},
+                                new String[]{"p1", "p4", "p1", "p3", "p2", "p2"},
+                                new String[]{"0:3", "3:6", "6:8", "8:12", "12:15", "15:18"}
+                        );
+                    }
+
+                }
+                catch (Exception er1) {
                     er1.printStackTrace();
                 }
             }
@@ -206,19 +278,51 @@ public class HelloApplication extends Application {
         window.showAndWait();
 
     }
-    public  void gantt_display() {
+    public  void gantt_display(String[]arr,String[]processes,String[]duration) throws FileNotFoundException {
+        System.out.println("before");
+
+        JFreeChart chart = ChartFactory.createGanttChart("Scheduler", "Processes", "Time", createDataset(arr,processes,duration),
+                true, true, false);
+        System.out.println("after");
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        try{
+            ChartUtilities.saveChartAsJPEG(new File("/Users/Holmez/Desktop/os/chart.jpeg"),chart,500,300);
+
+        }
+        catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        BorderPane layout = new BorderPane();
-        Scene scene = new Scene(layout, 400, 400);
+        Image image = new Image(new FileInputStream("/Users/Holmez/Desktop/os/chart.jpeg"));
+
+        //Setting the image view
+        ImageView imageView = new ImageView(image);
+
+        //Setting the position of the image
+        imageView.setX(50);
+        imageView.setY(25);
+
+        //setting the fit height and width of the image view
+        imageView.setFitHeight(455);
+        imageView.setFitWidth(500);
+
+        //Setting the preserve ratio of the image view
+        imageView.setPreserveRatio(true);
+
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root, 600, 500);
+
         window.setScene(scene);
-        window.showAndWait();
+        window.show();
 
     }
     public int arrIndex(String[]arr,String s){
-        int res = -1;
+        int res = 0;
         for(int i = 0; i < arr.length;i++){
-            if(arr[i] == s){
+            if(arr[i].equals(s)){
                 res = i;
                 break;
             }
@@ -229,15 +333,20 @@ public class HelloApplication extends Application {
 
         TaskSeriesCollection dataset = new TaskSeriesCollection();
         TaskSeries[] series = new TaskSeries[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            series[i]=new TaskSeries(arr[i]);
+        }
         for(int i = 0; i < processes.length; i++){
-            series[arrIndex(arr,processes[i])].add (new Task(processes[i],
+            series[arrIndex(arr,processes[i])].add (new Task(Integer.toString(i),
                     new SimpleTimePeriod(Integer.parseInt(duration[i].substring(0,duration[i].indexOf(":"))),
                             Integer.parseInt(duration[i].substring(duration[i].indexOf(":")+1)))));
         }
-        for(int i = 0; i < arr.length; i++){
-            dataset.add(series[i]);
-        }
+       for(int i = 0; i < arr.length; i++){
+           dataset.add(series[i]);
+       }
+
         return dataset;
+
     }
 
     public static void main(String[] args) {
